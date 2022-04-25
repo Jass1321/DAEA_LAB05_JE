0169dd0e0e5fb7cb0f00230cfea4fcd9ba195a2e
USUARIO <USUARIO@DESKTOP-IPA0A59>
2022-04-25 00:24:40 -0500

    Agregar archivos de proyecto.

---
+++ DAEA_LAB05_JE/ListaCategoria.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace S5_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para ListaCategoria.xaml
+    /// </summary>
+    public partial class ListaCategoria : Window
+    {
+        public ListaCategoria()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConsultar_Click(object sender, RoutedEventArgs e)
+        {
+            Cargar();
+        }
+
+        private void Cargar()
+        {
+            BCategoria bCategoria = null;
+            try
+            {
+                //0:Listar todas las categorias
+                bCategoria = new BCategoria();
+                dgvCategoria.ItemsSource = bCategoria.Listar(0);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Comunicarse con el admin -> {ex}");
+            }
+            finally
+            {
+                bCategoria = null;
+            }
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        {
+            //se coloca 0 pq es nuevo
+            ManCategoria manCategoria = new ManCategoria(0);
+            manCategoria.ShowDialog();
+            Cargar();
+        }
+
+        private void dgvCategoria_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            int idCategoria = 0;
+            var item = (Categoria)dgvCategoria.SelectedItem;
+            if (null == item) return;
+            idCategoria = Convert.ToInt32(item.IdCategoria);
+
+            ManCategoria manCategoria = new ManCategoria(idCategoria);
+            manCategoria.ShowDialog();
+            Cargar();
+
+        }
+
+    }
+}
---
+++ DAEA_LAB05_JE/ManCategoria.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Business;
+using Entity;
+
+namespace S5_WPF
+{
+    /// <summary>
+    /// Lógica de interacción para Categoria.xaml
+    /// </summary>
+    public partial class ManCategoria : Window
+    {
+        public int ID { get; set; }
+
+        public ManCategoria(int Id)
+        {
+            InitializeComponent();
+            ID = Id;
+
+            if (ID > 0)
+            {
+                BCategoria bCategoria = new BCategoria();
+                List<Categoria> categorias = new List<Categoria>();
+                categorias = bCategoria.Listar(ID);
+
+                if (categorias.Count > 0)
+                {
+                    lblID.Content = categorias[0].IdCategoria.ToString();
+                    txtNombre.Text = categorias[1].NombreCategoria;
+                    txtDescripcion.Text = categorias[2].Descripcion;
+                }
+            }
+        }
+
+        private void btnGrabar_Click(object sender, RoutedEventArgs e)
+        {
+            BCategoria bCategoria = null;
+            bool result = true;
+
+            try
+            {
+                //listar todas las categorias
+                bCategoria = new BCategoria();
+
+                if (ID > 0)
+                {
+                    result = bCategoria.Actualizar(new Categoria
+                    {
+                        IdCategoria = ID,
+                        NombreCategoria = txtNombre.Text,
+                        Descripcion = txtDescripcion.Text
+                    });
+                }
+                else
+                {
+                    result = bCategoria.Insertar(new Categoria
+                    {
+                        NombreCategoria = txtNombre.Text,
+                        Descripcion = txtDescripcion.Text
+                    });
+                }
+
+                if (!result)
+                {
+                    MessageBox.Show("Comunicarse con el Administrador");
+                }
+
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Comunicarse con el Administrador -> {ex}");
+            }
+            finally
+            {
+                bCategoria = null;
+            }
+        }
+
+        private void btnCerrar_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
